81c67be423a0352eff4175bb553fcb56e9178a19
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-04-21 12:56:31 +0100

    add the createExam page for test

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -7,4 +7,13 @@ const router = express.Router();
 router.get('/home',authenticate, authorizeRole("Teacher"), (req, res)=>{
     res.render('teacher/home', { user: req.user });
 })
+
+router.get('/createExam', authenticate, authorizeRole('Teacher'), (req, res) =>{
+    res.render('teacher/createExam',{exam:{
+        title:'test',
+        description: 'just take the test',
+        duration:60,
+        group: 'Mip 4'
+    }});
+})
 export default router;
