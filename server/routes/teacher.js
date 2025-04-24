82e9ac48ed4f68da90d115775f4558fd7483d0ff
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-04-24 23:45:57 +0100

    create controllers and routes to andle profile updates and password change

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -3,6 +3,8 @@ import { authenticate, authorizeRole } from '../middleware/authMiddleWare.js';
 import createExam from '../controllers/createExam.js';
 import teacherHome from '../controllers/teacherHome.js'
 
+import Exam from '../models/exams.js';
+
 const router = express.Router();
 
 router.get('/home',authenticate, authorizeRole("Teacher"), teacherHome);
@@ -12,6 +14,68 @@ router.get('/createExam',authenticate, authorizeRole("Teacher"), (req, res) =>{
 })
 
 router.post('/createExam', authenticate, authorizeRole('Teacher'),createExam);
+
+router.get('/myExams', authenticate, authorizeRole('Teacher'), (req, res)=>{
+    res.render('teacher/teacherExams',{user:req.user});
+})
+
+router.post('/myExams', authenticate, authorizeRole('Teacher'),async (req,res)=>{
+    const teacherId = req.user.userId;
+    const exams = await Exam.find({teacher_id: teacherId});
+    console.log(teacherId);
+    if(exams.length === 0){
+        res.status(500).json({
+            message:"You don't have any exams Yet"
+        })
+    }else{
+        const cleanExams = exams.map(e=>{
+            const exam ={
+                examId: e._id,
+                title:e.title,
+                description: e.description,
+                group: e.group,
+                status: e.status,
+                accesstoken: e.accessToken
+            }
+            return exam;
+        })
+        console.log(cleanExams);
+        res.status(201).json(cleanExams);
+    }
+})
+
+router.delete('/deleteExam', authenticate, authorizeRole('Teacher'),async (req, res)=>{
+    const examId = req.body.examId;
+    try{
+        const exam = await Exam.findOneAndDelete({_id:examId});
+        if(!exam){
+            res.status(400).json({message:"No exam Found", type:"danger"});
+        }else{
+            res.status(201).json({message:"exam deleted", type:"success"})
+        }
+    }catch(err){
+        res.status(400).json({message:"Sorry Something went wrong", type:"danger"});
+    }
+})
+
+router.post('/changeExamsStat', authenticate, authorizeRole('Teacher'), async (req,res)=>{
+    const teacherId = req.user.userId;
+    const {examId, currentStat, newStat} = req.body;
+    console.log(teacherId);
+    if(!teacherId || !examId || !currentStat || !newStat){
+        res.status(400).json({
+            message:"Something went Wrong changing the status",
+            type: "danger"
+        })
+        return;
+    }else{
+        await Exam.findOneAndUpdate({_id:examId,teacher_id:teacherId},{$set:{status:newStat}});
+        res.status(201).json({
+            message:`Exam is ${newStat}`,
+            type:'success'
+        })
+    }
+})
 
 
 export default router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import userRegister from '../controllers/registerUser.js';
+import User from '../models/user.js';
+import { authenticate } from '../middleware/authMiddleWare.js';
+import { updateProfile, changePassword } from '../controllers/updateUser.js';
 
 import loginUser from '../controllers/loginUser.js';
 const router = express.Router();
 
-router.post('/register', userRegister)
+router.post('/register', userRegister);
 
-router.post('/login', loginUser)
+router.post('/login', loginUser);
+router.get('/profile',authenticate, (req, res) =>{
+    // get teacher data from database
+    const userId = req.user.userId;
+    User.findById(userId).then((user) => {
+        if (!user) {
+            return res.render('auth/login');
+        }
+        // Render the profile page with teacher data
+        console.log(user);
+        res.render(`${user.role}/profile.ejs`, {user})
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send('Server error');
+    });
+})
+router.post('/updateProfile', authenticate, updateProfile)
+
+router.post('/changePassword',authenticate, changePassword);
 export default router;
 
 
