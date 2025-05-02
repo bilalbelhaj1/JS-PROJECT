efaf37a7bbc8e9ca9054473561a0d37cde1caef3
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-05-02 14:00:43 +0100

    exam link modal

--- server/controllers/createExam.js
+++ server/controllers/createExam.js
@@ -13,10 +13,12 @@ const createExam = async (req,res)=>{
     try{
         const exam = new Exam(examToSave);
         await exam.save();
+        console.log(accessToken);
         res.status(201).json({
             accessToken
         })
     }catch(err){
+        console.log(err);
         res.status(400).json({
             message: "Something went wrong"
         })
